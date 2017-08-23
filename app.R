665848c22e8e925a84691e0ec90a69064f601ae2
drwilkins <m.r.wilkins06@gmail.com>
2017-08-23 10:13:29 -0500

    Real progress, using reactiveValues

--- app.R
+++ app.R
@@ -14,36 +14,33 @@ ui <- fluidPage(
 #>User toggles
    fluidRow(
     column(1,radioButtons("axlab","Label Format:",c("Exponential"="exp","Integer"="int"))),
-    column(2,selectInput("highlight","Which family to highlight?",choices=c("none",fams),selected="none")),
-     column(6,
-       p("Selected Point(s):"),
-     tableOutput("click_info"))
+    column(2,selectInput("highlight","Which family to highlight?",choices=c("none",fams),selected="none"))
+     
      
     ),#end fluidRow 1
 ##############
 #> Graphical Output 
    fluidRow(
           column(5,
-            plotOutput("plot1", height = 500,width=500,click="plot1click")),
-     verbatimTextOutput("clue")#main MTE plot
+            plotOutput("plot1", height = 500,width=500,click="plot1click"))#main MTE plot
         
-      )#end fluidRow 2
+      ),#end fluidRow 2
+column(6,
+       h5("Selected Point(s):"),
+     tableOutput("click_info")),
+  verbatimTextOutput("clue")
 )#End ui
 
 
 #*********************************************************************
 server <- function(input, output) {
-#get user point click
-clicked<-NA
 
-output$click_info<-renderTable({clk<-nearPoints(df=birds,coordinfo=input$plot1click,xvar="log10mass",yvar="log10Hz",addDist=F,threshold=3)[,c("Taxon","family","fam","F_Hz","body_mass")] 
-  names(clk)<-c("Genus_species","Scientific Fam", "Common Fam","Freq (Hz)","Mass (g)")
-  clk
-})
-
+#store reactive list to access outside reactive functions
+vals<-reactiveValues(clicked=NULL)
+  
   output$plot1 <- renderPlot({
-    
-    g0<-ggplot(birds,aes(y=log10Hz,x=log10mass))+geom_point()+geom_smooth(method="lm")+xlab("Body Mass (g)")+ylab("Vocal Frequency (Hz)")+theme_linedraw()+theme(axis.text=element_text(size=16),axis.title=element_text(size=16,face="bold"))
+#Initial plot definition (gets modified by user input)
+  g0<-ggplot(birds,aes(y=log10Hz,x=log10mass))+geom_point(size=3,stroke=1.5,shape=21,alpha=.75)+geom_smooth(method="lm")+xlab("Body Mass (g)")+ylab("Vocal Frequency (Hz)")+theme_linedraw()+theme(axis.text=element_text(size=16),axis.title=element_text(size=16,face="bold"))
 
   #if user selects Exponential Label format, change axis labels accordingly, else display as integers
 if(input$axlab=="exp"){
@@ -51,27 +48,33 @@ if(input$axlab=="exp"){
   }else{
   g0<-g0+scale_y_continuous(breaks=seq(0,4,1),labels=sapply(seq(0,4,1),function(x)10^x),limits=c(2.,4))+scale_x_continuous(breaks=seq(0,4.1,1),labels=sapply(seq(0,4.1,1),function(x)10^x),limits=c(0,4.25))}  
   
-if(input$highlight!="none"){#if a family is selected, make those points red diamonds
+#if a family is selected, make those points red diamonds
+if(input$highlight!="none"){
   highlighted=subset(birds,fam==input$highlight)
-  g1=g0+geom_point(data=highlighted,aes(x=log10mass,y=log10Hz),col="#FF0F1B",size=4,shape=18)}else{g1<-g0}
+  g1=g0+geom_point(data=highlighted,aes(x=log10mass,y=log10Hz),col="#FF0F1B",size=3,shape=5,stroke=1.5,alpha=.75)}else{g1<-g0}
   
 
 
-# If point clicked, distinguish that point
-
-clk2<-isolate(nearPoints(df=birds,coordinfo=input$plot1click,xvar="log10mass",yvar="log10Hz",addDist=F,threshold=3)[,c("Taxon","family","fam","F_Hz","body_mass")] )
-output$clue<-renderPrint(clk2)
-
-#  if(nrow(clicked)>0){
-  #   clicked2<-birds[which(clicked$Genus_species%in%birds$Taxon),]
-  #   g1<-g1+geom_point(df=clicked2,aes(x=log10mass,y=log10Hz),col="yellow")
+  # #If point selected, distinguish that point
+  # if(length(vals$clicked)>0){
+  #   clickeddf<-birds[which(birds$Taxon%in%vals$clicked[,"Taxon"]),]
+  #   g1<-g1+geom_point(data=clickeddf,aes(x=log10mass,y=log10Hz),col="yellow")
   #   }
   return(g1)
   
     }) #end renderPlot
   
-
+#get user point click
+observeEvent(input$plot1click, {
+  clk<-nearPoints(birds,coordinfo=input$plot1click,xvar="log10mass",yvar="log10Hz",addDist=F,threshold=5)[,c("Taxon","family","fam","F_Hz","body_mass")]
+  vals$clicked<-clk#assigned to reactiveValue object
+  #render Table & rename columns for output
+output$click_info<-renderTable({
+  names(clk)<-c("Genus_species","Scientific Fam", "Common Fam","Freq (Hz)","Mass (g)")
+clk})
+})
   
+output$clue<-renderPrint(vals$clicked)
   }#end server
 
 shinyApp(ui, server)
